9cae7c6fc7f30114917b29cf085d1ff83dde6611
Tyundong <122962820+Tyundong@users.noreply.github.com>
2024-04-29 23:20:54 +0800

    Update dag2file.go

--- dag2file.go
+++ dag2file.go
@@ -1,7 +1,63 @@
 package merkledag
 
-// Hash to file
+import (
+	"encoding/json"
+	"strings"
+)
+
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
+	value, err := store.Get(hash)
+	if err != nil {
+		return nil
+	}
+	var obj Object
+	err = json.Unmarshal(value, &obj)
+	if err != nil {
+		return nil
+	}
+	parts := strings.Split(path, "/")
+	if len(obj.Links) > 0 {
+		for i, link := range obj.Links {
+			if link.Name == parts[0] {
+				if len(parts) == 1 {
+					if string(obj.Data[i][0]) == "list" {
+						return retrieveList(store, link.Hash)
+					} else if string(obj.Data[i][0]) == "blob" {
+						value, err := store.Get(link.Hash)
+						if err != nil {
+							return nil
+						}
+						return value
+					}
+					return link.Hash
+				}
+				return Hash2File(store, link.Hash, strings.Join(parts[1:], "/"), hp)
+			}
+		}
+		return nil
+	}
 	return nil
+}
+func retrieveList(store KVStore, hash []byte) []byte {
+	var data []byte
+	var nextHash []byte = hash
+
+	for nextHash != nil {
+		value, err := store.Get(nextHash)
+		if err != nil {
+			return nil
+		}
+		var node ListNode
+		err = json.Unmarshal(value, &node)
+		if err != nil {
+			return nil
+		}
+		fileData, err := store.Get(node.Hash)
+		if err != nil {
+			return nil
+		}
+		data = append(data, fileData...)
+		nextHash = node.Next
+	}
+	return data
 }
